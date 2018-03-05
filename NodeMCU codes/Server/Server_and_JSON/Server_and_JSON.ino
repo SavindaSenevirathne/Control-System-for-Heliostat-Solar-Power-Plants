78fcdc22b49fe385495cfac8d64dd48aca1e40d1
Savinda Senevirathne <savindadilsara@gmail.com>
2018-03-05 21:59:32 +0530

    Node server

---
+++ NodeMCU codes/Server/Server_and_JSON/Server_and_JSON.ino
@@ -0,0 +1,117 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "Senevirathne";
+const char* password = "SuduBruno6694";
+
+ESP8266WebServer server(80);
+
+const int led = 13;
+
+void handleRoot() {
+  digitalWrite(led, 1);
+  server.send(200, "text/plain", "hello from esp8266!");
+  digitalWrite(led, 0);
+}
+
+void handleNotFound(){
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+}
+
+
+void sendJSON(){
+    StaticJsonBuffer<300> JSONbuffer;   //Declaring static JSON buffer
+    JsonObject& JSONencoder = JSONbuffer.createObject(); 
+ 
+    JSONencoder["_token"] = "AMyrhDMe3720QqoLxIV4wZkbZi5TgiAxybFKGABR";
+ 
+    JsonArray& values = JSONencoder.createNestedArray("values"); //JSON array
+    values.add(20); //Add value to array
+    values.add(21); //Add value to array
+    values.add(23); //Add value to array
+ 
+    JsonArray& timestamps = JSONencoder.createNestedArray("timestamps"); //JSON array
+    timestamps.add("10:10"); //Add value to array
+    timestamps.add("10:20"); //Add value to array
+    timestamps.add("10:30"); //Add value to array
+ 
+    char JSONmessageBuffer[300];
+    JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
+    Serial.println(JSONmessageBuffer);
+ 
+    HTTPClient http;    //Declare object of class HTTPClient
+ 
+    http.begin("http://192.168.1.6:8000/api/postjson");      //Specify request destination
+    http.addHeader("Content-Type", "application/json");  //Specify content-type header
+//    http.addHeader("Authorization", "AMyrhDMe3720QqoLxIV4wZkbZi5TgiAxybFKGABR");
+    
+    int httpCode = http.POST(JSONmessageBuffer);   //Send the request
+
+    String payload = http.getString();                                        //Get the response payload
+ 
+    Serial.println(httpCode);   //Print HTTP return code
+    Serial.println(payload);    //Print request response payload
+ 
+    http.end();  //Close connection
+    delay(30000);//delay 30s
+
+}
+
+
+
+void setup(void){
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 0);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+
+  server.on("/inline", [](){
+    server.send(200, "text/plain", "this works as well");
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void){
+  sendJSON();
+  server.handleClient();
+}
