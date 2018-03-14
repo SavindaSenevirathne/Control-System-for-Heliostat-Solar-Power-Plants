7d5dfd437ec5acb510bf0a4f42752859f92dd26d
Savinda Senevirathne <savindadilsara@gmail.com>
2018-03-14 13:29:41 +0530

    New landing page

--- Laravel App/app/Http/Controllers/SessionsController.php
+++ Laravel App/app/Http/Controllers/SessionsController.php
@@ -1,11 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Plant;
 
 class SessionsController extends Controller {
 	//
 	public function index() {
 
 		return view('index');
+	}
+	public function home()
+	{
+		$plants = Plant::all();
+		return view('welcome', compact('plants'));
 	}
 }
--- Laravel App/routes/web.php
+++ Laravel App/routes/web.php
@@ -10,9 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
  */
-Route::get('/', function() {
-    return view('welcome');
-});
+Route::get('/', 'SessionsController@home');
 Route::get('/home', 'SessionsController@index');
 Route::get('plants', 'PlantsController@index');
 Route::get('plants/{plant}', 'PlantsController@show');
